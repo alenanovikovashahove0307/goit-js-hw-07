c9c757d37f1ddbaaf29ed900bbc108960bb9dd95
alenanovikovashahove0307 <novikova-fin@ukr.net>
2023-05-07 12:39:29 +0300

    Додано розмітку і модалку

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const list = document.querySelector(".gallery");
+const imgEl = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item")><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a></li>`
+  )
+  .join("");
 
+list.insertAdjacentHTML("beforeend", imgEl);
+galleryUl.addEventListener("click", onGalleryItemClick);
+function onGalleryItemClick(event) {
+    // if (event.target.nodeName !=="IMG") {
+    //     return
+    // }
+  event.preventDefault();
+  const instance = basicLightbox.create(
+    ` <div class="modal">
+        <img src="${event.target.dataset.source}" width="800" height="600">
+      </div>`
+  );
+  instance.show();
+  const modalElement = document.querySelector('.modal');
+  modalElement.addEventListener("click", () => {
+    instance.close();
+  });
+    const escapePress = basicLightbox.create(html, {
+     onShow: () => {
+      window.addEventListener("keydown", onModalKeyDown);   
+    },
+     onClose: () => {
+      window.removeEventListener("keydown", onModalKeyDown);
+    },
+  })
+  function onModalKeyDown(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
 console.log(galleryItems);
