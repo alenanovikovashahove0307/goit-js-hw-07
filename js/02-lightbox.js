0d50e003db9035f1f648e35f26b205158e5e596b
alenanovikovashahove0307 <novikova-fin@ukr.net>
2023-05-07 13:58:12 +0300

    Додано завдання 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const list = document.querySelector(".gallery");
+const imgEl = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item")><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}"  alt="${item.description}"/></a></li>`
+  )
+  .join("");
 
+list.insertAdjacentHTML("beforeend", imgEl);
+const lightbox = new SimpleLightbox(".gallery", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
 console.log(galleryItems);
